f6f0e3cc57c8bdf11e0e5185e2e88c0ceca4a869
Suissa <jnascimento@gmail.com>
2014-09-09 15:24:59 -0300

    Subindo exemplo de código modular

---
+++ app.js
@@ -0,0 +1,32 @@
+var http = require("http"),
+  mongoose = require('mongoose'),
+  Beer = require('./models/beer')(mongoose),
+  Controller = require('./controllers/beers')(Beer),
+  msg = '';
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
+
+  var url = req.url;
+
+  switch (url) {
+  case '/create':
+    Controller.create(req, res);
+    break;
+  case '/retrieve':
+    Controller.retrieve(req, res);
+    break;
+  case '/update':
+    Controller.update(req, res);
+    break;
+  case '/delete':
+    Controller.delete(req, res);
+    break;
+  default:
+    res.end('Rota não encontrada');
+  }
+
+}).listen(3000);
+console.log('Server running at http://localhost:3000/');
+
+
